849d4d66b73795f1b04a45a43b45297abe7e0946
Simran Sinha <161783369+SimranaSinha@users.noreply.github.com>
2026-03-05 16:26:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-const yearEl = document.getElementById("year");
-if (yearEl) yearEl.textContent = new Date().getFullYear();
-
 const navToggle = document.getElementById("navToggle");
-const navLinks = document.querySelector(".nav-links");
+const navLinks = document.getElementById("navLinks");
 
 if (navToggle && navLinks) {
   navToggle.addEventListener("click", () => {
     navLinks.classList.toggle("open");
   });
 
-  // Close menu after clicking a link (mobile)
   navLinks.querySelectorAll("a").forEach(a => {
     a.addEventListener("click", () => navLinks.classList.remove("open"));
   });
 }
+
+const yearEl = document.getElementById("year");
+if (yearEl) yearEl.textContent = new Date().getFullYear();
